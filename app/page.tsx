1b8ca7f155fcf480fa9ce92683b5ee04d7c828fe
0X1622 <shriramdhumal2476@gmail.com>
2025-04-08 17:18:15 +0530

    Fix TypeScript final

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default async function Home({
         </div>
         {/* Render papers directly */} 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5">
-          {papers.map((paper) => (
+          {papers.reverse().map((paper) => (
             <PaperCard key={paper.id} paper={paper} />
           ))}
         </div>
